c3e944831984fba77bfe16d817ea94d683aae0f2
Shreya <banshalshreya29@gmail.com>
2024-05-27 23:29:39 +0530

    mern commit

---
+++ Insta-Clone/src/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./homepage.css"
+import sidenav from './navigation/sidenav'
+import Timeline from './timeline/timeline'
+
+function Homepage() {
+  return (
+    <div className='Homepage'>
+        <div className='Homepage_nav'>
+
+        </div>
+        <div className='Homepage_timeline'>
+
+        </div>
+    </div>
+  )
+}
+
+export default homepage
